5de86d93df2418b2fcc6e1071b8ce3223065a70c
maokailu <1113901189@qq.com>
2018-03-31 18:08:13 +0800

    add progress-bar

---
+++ src/utils/progress-bar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+export default class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+  };
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
