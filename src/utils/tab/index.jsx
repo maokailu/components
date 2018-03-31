986f0a8a32d490522f5996e7de8431c8b8005e51
maokailu <1113901189@qq.com>
2018-03-31 18:04:20 +0800

    add tab folder

---
+++ src/utils/tab/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+export default class Tab extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+  };
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
