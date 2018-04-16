e1f8f44db9bc61807abbba827f4155eeb1ce16b8
maokailu <1113901189@qq.com>
2018-04-16 21:25:56 +0800

    eslint

--- src/utils/2048/Tile.jsx
+++ src/utils/2048/Tile.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './style.scss';
-import classNames from 'classnames';
 
 export default class Tile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    let tile = 'tile-' + this.props.num;
-    return (
-      <div className = {'tile ' + tile}>
-        {this.props.num}
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        let tile = 'tile-' + this.props.num;
+        return <div className={'tile ' + tile}>{this.props.num}</div>;
+    }
 }
