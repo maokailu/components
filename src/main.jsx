8b1dbcb814ebe974f0448dcf577b67d95ee19890
maokailu <1113901189@qq.com>
2018-07-09 20:52:04 +0800

    滑动组件

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import App from './air-booking/detail';
-import App from './utils/2048';
+import App from './utils/swiper/tab';
 import './resources/global.scss';
 import './main.scss';
 // import './utils/adaptation';
