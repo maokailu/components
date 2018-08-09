5f3d8f56f4628b5699e42c6e55e96f6ea84c1ca7
maokl <maokl@ctrip.com>
2018-08-09 11:16:33 +0800

    carousel:appendChild

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.scss';
-// 不断左移，左移后数组shift and push
-let currentIndex = 1;
+// 不断左移，左移后数组push
+let currentIndex = 0;
 export default class Carousel extends React.Component {
     constructor(props) {
         super(props);
@@ -10,34 +10,34 @@ export default class Carousel extends React.Component {
         arr: [1, 2, 3]
     };
     componentDidMount() {
-        this.timerID = setInterval(() => {
-            const tab = document.getElementById('tab');
-            tab.style.transform =  `translateX(-${currentIndex * 100}vw)`;
-            tab.style.transition = 'transform 0.1s';
-            // const arr = this.state.arr;
-            // arr.push(arr.shift());
-            // this.setState({
-            //     arr: arr
-            // });
-            currentIndex++;
-            const prev = tab.children[0];
-            tab.appendChild(prev);
-            tab.removeChild(prev);
+        const tab = document.getElementById('tab');
+        setInterval(() => {
+            ++currentIndex;
+            tab.style.transform =  `translateX(-${100 * currentIndex}vw)`;
+            tab.style.transition = 'transform 1s';
+            setTimeout(()=>{
+                const newchild = document.createElement('div');
+                newchild.innerText = tab.childNodes[currentIndex - 2].innerText;
+                // 如果文档树中已经存在了 newchild，它将从文档树中删除
+                tab.appendChild(newchild);
+            }, 1000);
         }, 1000);
     }
     componentWillUnmount() {
-        clearInterval(this.timerID);
+        // clearInterval(this.timerID);
     }
     render() {
         const arr = this.state.arr;
         return (
-            <div className="tab1" id="tab">
-                {/* {arr.map((item, index) => (
-                    <div key={index}>{item}</div>
-                ))} */}
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
+            <div className="wrapper">
+                <div className="tab1" id="tab">
+                    {/* {arr.map((item, index) => (
+                        <div key={index}>{item}</div>
+                    ))} */}
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
             </div>
         );
     }
